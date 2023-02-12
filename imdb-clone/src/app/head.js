16123263def71164fb124d24ee0a293f6e1e2bfc
riku-42 <otogurohana@gmail.com>
2023-02-12 22:31:07 +0800

    install nextjs

---
+++ imdb-clone/src/app/head.js
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>IMDb clone</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="This is the IMDb clone site." />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
